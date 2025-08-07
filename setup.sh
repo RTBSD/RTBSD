51dd757b70d948a216176002ac5d9d80e3f31731
zhugengyu <zhugengyu2023@gmail.com>
2025-08-07 18:58:30 +0800

    speed up http pull

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 git clone https://github.com/RTBSD/LibBSD.git -b main ./libbsd
 git clone https://github.com/RTBSD/cheribuild.git -b main ./tools/cheribuild
-git clone https://github.com/RTBSD/freebsd-src.git -b releng/14.3 ./upstream/freebsd
-git clone https://github.com/RTBSD/NetBSD-src.git -b netbsd-10 ./upstream/netbsd
-git clone https://github.com/RTBSD/rt-thread.git -b master ./rtos/rt-thread
+git clone https://github.com/RTBSD/freebsd-src.git -b releng/14.3 ./upstream/freebsd --depth=1
+git clone https://github.com/RTBSD/NetBSD-src.git -b netbsd-10 ./upstream/netbsd --depth=1
+git clone https://github.com/RTBSD/rt-thread.git -b master ./rtos/rt-thread --depth=1
