8d93c43c83a51b79f96ddc46c104bab02ecd3739
zhugengyu <zhugengyu2023@gmail.com>
2025-08-08 18:04:36 +0800

    add freebsd qemu debug

--- makefile
+++ makefile
@@ -6,3 +6,10 @@ export HOST_UNAME_P!=  uname -p
 include $(RTBSD_DIR)/tools/freebsd_aarch64.mk
 include $(RTBSD_DIR)/tools/netbsd_aarch64.mk
 include $(RTBSD_DIR)/tools/rtthread_aarch64.mk
+include $(RTBSD_DIR)/tools/baremetal_raspi3.mk
+
+qemu_aarch64_info:
+	@qemu-system-aarch64 --machine help
+	@qemu-system-aarch64 --device help
+	@qemu-system-aarch64 --cpu help
+	@qemu-system-aarch64 --version
--- tools/freebsd_aarch64.mk
+++ tools/freebsd_aarch64.mk
@@ -11,10 +11,12 @@ FREEBSD_ROOTFS := ufs
 FREEBSD_HOSTNAME := rtbsd
 FREEBSD_QEMU_EFI := /usr/share/qemu-efi-aarch64/QEMU_EFI.fd
 FREEBSD_SRC_DIR := $(RTBSD_DIR)/upstream/freebsd
-FREEBSD_ROOTFS_DIR := $(RTBSD_DIR)/build/freebsd-aarch64-build$(RTBSD_DIR)/freebsd/arm64.aarch64/sys/$(FREEBSD_KERNCONFIG)
+FREEBSD_ROOTFS_DIR := $(RTBSD_DIR)/build/freebsd-aarch64-build$(RTBSD_DIR)/upstream/freebsd/arm64.aarch64/sys/$(FREEBSD_KERNCONFIG)
 
 llvm_x86_64_debian_toolchain:
-	@wget https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+	@if [ ! -f "clang+llvm-18.1.8-x86_64-linux-gnu-ubuntu-18.04.tar.xz" ]; then \
+		@wget https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-x86_64-linux-gnu-ubuntu-18.04.tar.xz; \
+	fi
 	@mkdir -p $(RTBSD_DIR)/tools/llvm-x86_64
 	@tar -xvf clang+llvm-18.1.8-x86_64-linux-gnu-ubuntu-18.04.tar.xz \
 		-C $(RTBSD_DIR)/tools/llvm-x86_64 \
@@ -37,6 +39,7 @@ freebsd_aarch64_image:
 		--skip-update \
 		$(FREEBSD_VERBOSE)
 	@cp $(RTBSD_DIR)/build/output/freebsd-aarch64.img . -f
+	@qemu-img resize freebsd-aarch64.img 4G
 
 freebsd_aarch64_run:
 	@echo "Run FreeBSD(AARCH64)"
@@ -44,6 +47,51 @@ freebsd_aarch64_run:
 		-drive if=none,file=freebsd-aarch64.img,id=hd0 -device virtio-blk-device,drive=hd0 \
 		-netdev type=user,id=net0 -device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 \
 		-bios $(FREEBSD_QEMU_EFI) -nographic
+
+# setup nameserver after bootup
+# 	echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+# 	echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+freebsd_aarch64_net_run:
+	@echo "Run FreeBSD(AARCH64) with Network"
+	@qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 4g \
+		-drive if=none,file=freebsd-aarch64.img,id=hd0 -device virtio-blk-device,drive=hd0 \
+		-netdev user,id=net0,hostfwd=tcp::2222-:22 \
+		-device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 \
+		-bios $(FREEBSD_QEMU_EFI) -nographic
+
+freebsd_aarch64_pci_run:
+	@echo "Run FreeBSD(AARCH64) with PCIe"
+	@qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 4g \
+		-drive if=none,file=freebsd-aarch64.img,id=hd0 -device virtio-blk-device,drive=hd0 \
+		-netdev type=user,id=net0 -device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 \
+		-device pci-bridge,chassis_nr=1,id=pci-bridge-1,bus=pcie.0,addr=0x1 \
+		-device qemu-xhci,id=xhci,bus=pci-bridge-1,addr=0x1 \
+		-device usb-hub,bus=xhci.0,port=1 \
+		-device usb-mouse,bus=xhci.0,port=1.1 \
+		-bios $(FREEBSD_QEMU_EFI) -nographic
+
+freebsd_aarch64_xhci_run:
+	@echo "Run FreeBSD(AARCH64) with XHCI"
+	@qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 4g \
+		-drive if=none,file=freebsd-aarch64.img,id=hd0 -device virtio-blk-device,drive=hd0 \
+		-netdev type=user,id=net0 -device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 \
+		-device qemu-xhci,id=xhci \
+		-device usb-hub,bus=xhci.0,port=1 \
+		-device usb-mouse,bus=xhci.0,port=1.1 \
+		-bios $(FREEBSD_QEMU_EFI) -nographic
+
+freebsd_aarch64_debug:
+	@echo "Run FreeBSD(AARCH64) in debug mode"
+	@qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 4g \
+		-drive if=none,file=freebsd-aarch64.img,id=hd0 -device virtio-blk-device,drive=hd0 \
+		-netdev type=user,id=net0 -device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 \
+		-bios $(FREEBSD_QEMU_EFI) -nographic \
+		-s -S
+
+freebsd_aarch64_attach:
+	@echo "Attach FreeBSD(AARCH64) in debug mode"
+	@cp $(FREEBSD_ROOTFS_DIR)/kernel kernel -f
+	@gdb-multiarch -x ./tools/.gdbinit.freebsd.aarch64
 
 freebsd_aarch64_clean:
 	@echo "Clean FreeBSD(AARCH64)"
