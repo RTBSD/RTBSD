63375d34ec4c3a1e0758856441ee5458247aa88d
zhugengyu <zhugengyu2023@gmail.com>
2025-08-19 18:59:44 +0800

    move libbsd obj dir

--- tools/libbsd.mk
+++ tools/libbsd.mk
@@ -1,13 +1,23 @@
 export SHELL := /bin/bash
 
+LIBBSD_BUILD_DIR := $(RTBSD_DIR)/build/libbsd
+
 libbsd_fdt_aarch64:
-	@cd $(RTBSD_DIR)/libbsd && make -f makefile.gcc.aarch64 AARCH64=1 FDT=1 GDBSTUB=1 all
+	@cd $(RTBSD_DIR)/libbsd && \
+		make -f makefile.gcc.aarch64 \
+		AARCH64=1 FDT=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) all
 
 libbsd_acpi_aarch64:
-	@cd $(RTBSD_DIR)/libbsd && make -f makefile.gcc.aarch64 AARCH64=1 ACPI=1 GDBSTUB=1 all
+	@cd $(RTBSD_DIR)/libbsd && \
+		make -f makefile.gcc.aarch64 \
+		AARCH64=1 ACPI=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) all
 
 libbsd_fdt_aarch64_clean:
-	@cd $(RTBSD_DIR)/libbsd && make -f makefile.gcc.aarch64 AARCH64=1 FDT=1 GDBSTUB=1 clean
+	@cd $(RTBSD_DIR)/libbsd && \
+		make -f makefile.gcc.aarch64 \
+		AARCH64=1 FDT=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) clean
 
 libbsd_acpi_aarch64_clean:
-	@cd $(RTBSD_DIR)/libbsd && make -f makefile.gcc.aarch64 AARCH64=1 ACPI=1 GDBSTUB=1 clean
+	@cd $(RTBSD_DIR)/libbsd && \
+		make -f makefile.gcc.aarch64 \
+		AARCH64=1 ACPI=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) clean
