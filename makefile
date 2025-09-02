86dd02d171c981b3b033437cd797599e6ebf4d73
zhugengyu <zhugengyu2023@gmail.com>
2025-09-02 17:52:50 +0800

    remove unused repo

--- makefile
+++ makefile
@@ -2,14 +2,7 @@ export RTBSD_DIR := $(realpath $(CURDIR))
 export HOST_UNAME_S!=  uname -s
 export HOST_UNAME_R!=  uname -r
 export HOST_UNAME_P!=  uname -p
-export QEMU_TOOL_PATH := $(RTBSD_DIR)/build/output/qemu
-export QEMU_BIN_PATH := $(QEMU_TOOL_PATH)/bin
 
-include $(RTBSD_DIR)/tools/qemu.mk
 include $(RTBSD_DIR)/tools/freebsd.mk
 include $(RTBSD_DIR)/tools/netbsd.mk
 include $(RTBSD_DIR)/tools/libbsd.mk
-include $(RTBSD_DIR)/tools/rtthread.mk
-include $(RTBSD_DIR)/tools/baremetal_raspi3.mk
-include $(RTBSD_DIR)/tools/rtems.mk
-include $(RTBSD_DIR)/tools/nuttx.mk
--- tools/libbsd.mk
+++ tools/libbsd.mk
@@ -3,21 +3,18 @@ export SHELL := /bin/bash
 LIBBSD_BUILD_DIR := $(RTBSD_DIR)/build/libbsd
 
 libbsd_fdt_aarch64:
+	@mkdir -p $(LIBBSD_BUILD_DIR)
 	@cd $(RTBSD_DIR)/libbsd && \
 		make -f makefile.gcc.aarch64 \
-		AARCH64=1 FDT=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) all
+		AARCH64=1 FDT=1 GDBSTUB=1 PCI=1 \
+		OBJDIR=$(LIBBSD_BUILD_DIR) all
 
 libbsd_acpi_aarch64:
+	@mkdir -p $(LIBBSD_BUILD_DIR)
 	@cd $(RTBSD_DIR)/libbsd && \
 		make -f makefile.gcc.aarch64 \
-		AARCH64=1 ACPI=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) all
+		AARCH64=1 ACPI=1 GDBSTUB=1 PCI=1 \
+		OBJDIR=$(LIBBSD_BUILD_DIR) all
 
-libbsd_fdt_aarch64_clean:
-	@cd $(RTBSD_DIR)/libbsd && \
-		make -f makefile.gcc.aarch64 \
-		AARCH64=1 FDT=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) clean
-
-libbsd_acpi_aarch64_clean:
-	@cd $(RTBSD_DIR)/libbsd && \
-		make -f makefile.gcc.aarch64 \
-		AARCH64=1 ACPI=1 GDBSTUB=1 OBJDIR=$(LIBBSD_BUILD_DIR) clean
+libbsd_clean:
+	@rm $(LIBBSD_BUILD_DIR) -rf
